d917b35d08a3edea0afd0fe6db89f47f6759af23
csharp109 <stratocaster01>
2017-04-27 15:05:11 +0200

    start

---
+++ Opg01HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg01HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast navn");
+            string navn = Console.ReadLine();
+            navn = navn.ToUpper();
+            Console.WriteLine("Velkommen " + navn);
+            Console.WriteLine("Tryk på en tast for at fortsætte");
+            Console.ReadKey();
+            
+
+        }
+    }
+}
