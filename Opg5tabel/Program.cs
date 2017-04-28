1cc5582ff3876dece483f5fda0f7b753c7d576b7
csharp109 <stratocaster01>
2017-04-28 15:49:54 +0200

    ok

---
+++ Opg5tabel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg5tabel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x < 11; x++)
+
+            {
+                for (int y = 1; y < 11; y++)
+                {
+                    if (y < 10)
+                        {
+                        
+                        Console.Write((x*y) + " ");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine((y * x). + " ");
+                    }
+
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
