04fba51470d32d29e6e9a99c1b0396657cb69221
csharp109 <stratocaster01>
2017-04-28 10:58:44 +0200

    ok

--- opg2variabler/Program.cs
+++ opg2variabler/Program.cs
@@ -15,10 +15,60 @@ namespace opg2variabler
             Console.WriteLine(heltal);
             heltal--;
             Console.WriteLine(heltal);
-            heltal+= 20;
+            //lægger 20 til heltal
+            heltal += 20;
             Console.WriteLine(heltal);
+
+            ///**********************************************
+            ///
+
+            double kommatal = 12.5;
+            kommatal++;
+            Console.WriteLine(kommatal);
+            kommatal--;
+            Console.WriteLine(kommatal);
+            //ganger med 2
+            kommatal *= 2;
+            Console.WriteLine(kommatal);
             
+
+            ///**********************************************
+            ///
+            Filtyper ft = Filtyper.csv;
+            Console.WriteLine(ft);
+            Console.WriteLine((int) ft);
+
+            ///**********************************************
+            ///
+
+            DateTime dato = DateTime.Now;
+            //formatering af dato output
+            Console.WriteLine(dato.ToString("ddMMyy"));
+
+            ///**********************************************
+            ///
+
+            Person p;
+            p.Id = 1;
+            p.Navn = "Mikkel";
+            Console.WriteLine(p.Navn);
+
             Console.ReadKey();
         }
+
+        
+
+    }
+    public enum Filtyper
+    {
+        csv,
+        pdf,
+        txt
+    }
+
+    public struct Person
+    {
+        public int Id;
+        public string Navn;
     }
 }
