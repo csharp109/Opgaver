8ee36685d5bfd4e24c1275bb89a3d2e507ee10e1
csharp109 <stratocaster01>
2017-04-28 14:43:03 +0200

    ok

---
+++ Opg4strenge/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg4strenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fornavn = "Mikkel";
+            string efternavn = "Cronberg";
+            string samletnavn = "Mikkel Cronberg";
+            string navnStort = samletnavn.ToUpper();
+            string navnLille = samletnavn.ToLower();
+            string del = samletnavn.Substring(7, 4).ToLower();
+
+            string[] et = samletnavn.Split(' ');
+
+            Console.WriteLine(samletnavn);
+            Console.WriteLine(navnStort);
+            Console.WriteLine(navnLille);
+            Console.WriteLine(del);
+
+            foreach (var item in et)
+            {
+                Console.WriteLine(item);
+
+            }
+
+            Console.ReadKey();
+  
+        }
+    }
+}
