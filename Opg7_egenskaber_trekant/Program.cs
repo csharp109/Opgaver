a823538463ec461eb9ba08743d9c56860692ce78
csharp109 <stratocaster01>
2017-05-09 15:37:47 +0200

    ok

---
+++ Opg7_egenskaber_trekant/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg7_egenskaber_trekant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trekant t = new Trekant(4, 6);
+            Console.WriteLine(t);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Opg7_egenskaber_trekant/Trekant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg7_egenskaber_trekant
+{
+    class Trekant
+    {
+        public int Grundlinje { get; private set; }
+
+        public int Højde { get; private set; }
+
+   
+        public Trekant(int Grundlinje, int Højde)
+        {
+            this.Grundlinje = Grundlinje;
+            this.Højde = Højde;
+        }
+
+
+        public double Areal
+        {
+            get { return this.Grundlinje * this.Højde * 0.95; }
+        }
+
+    }
+}
