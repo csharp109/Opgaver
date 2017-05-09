4bfcad6a7b276589c5125b6d9def5d24c8fdfc1e
csharp109 <stratocaster01>
2017-05-09 14:35:57 +0200

    ok

---
+++ Opg6arv/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg6arv
+{
+    public class Person
+    {
+        public string Fornavn { get; set; }
+
+        public string Efternavn { get; set; }
+
+        public string FuldtNavn()
+        {
+            return $"{Fornavn} {Efternavn}";
+        }
+
+    }
+
+    public class Elev : Person
+    {
+        public string Klasselokale { get; set; }
+        
+    }
+
+    public class Instruktør : Person
+    {
+        public int NøgleId { get; set; }
+
+    }
+
+}
---
+++ Opg6arv/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg6arv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //
+
+            //Person p = new Person();
+            //Console.WriteLine(p.FuldtNavn{ Fornavn = "Jens", Efternavn = "Hansen"});
+            Person p = new Person() { Fornavn = "Jens", Efternavn = "Hansen" };
+            Console.WriteLine(p.FuldtNavn());
+
+            Elev e = new Elev() { Fornavn = "Jens", Efternavn = "Hansen", Klasselokale = "B" };
+            Console.WriteLine(e.FuldtNavn() + "dette er klasselokale " + e.Klasselokale);
+
+            Instruktør i = new Instruktør() { Fornavn = "Jens", Efternavn = "Hansen", NøgleId = 11 };
+            Console.WriteLine(i.FuldtNavn() + "dette er nøgleid" + i.NøgleId);
+
+            
+            Console.ReadKey();
+        }
+        
+    }
+}
