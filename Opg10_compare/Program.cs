0ba32a1cdcce21cde94ef9d25b4d364f0eecec71
admin <admin@AR-113-06>
2017-05-29 16:00:06 +0200

    ok

---
+++ Opg10_compare/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg10_compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Hund[] hunde = new Hund[4];
+            hunde[0] = new Hund() { Alder = 10, Navn = "Bulder" };
+            hunde[1] = new Hund() { Alder = 5, Navn = "Lady" };
+            hunde[2] = new Hund() { Alder = 8, Navn = "Sommer" };
+            hunde[3] = new Hund() { Alder = 25, Navn = "Nick" };
+            Array.Sort(hunde);
+            foreach (var item in hunde)
+            {
+                Console.WriteLine(item.Navn);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Hund : IComparable
+    {
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+
+      public int CompareTo(object obj)
+        {
+            Hund h = obj as Hund;
+            Console.WriteLine("h.alder= " + h.Alder);
+            Console.WriteLine("this.alder= " + this.Alder);
+
+            if (h.Alder > this.Alder)
+                return -1;
+            if (h.Alder < this.Alder)
+                return 1;
+
+            
+
+            return 0;
+
+        }
+
+
+    }
+}
